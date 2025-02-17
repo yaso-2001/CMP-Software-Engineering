6dc03b3a28aee4d6200c8530322720b376d75373
yaso-2001 <160578525+yaso-2001@users.noreply.github.com>
2025-02-17 08:10:06 +0200

    Update employee.js

--- Lab#1/BE/controllers/employee.js
+++ Lab#1/BE/controllers/employee.js
@@ -8,8 +8,32 @@ exports.getEmployees = async (req, res, next) => {
 
 // TODO
 exports.deleteEmployee = async (req, res, next) => {
+  const id = req.params.id;
+  employee.forEach((emp, index) => 
+  {
+    if (emp.id === id)
+    {
+      employee.splice(index, 1);
+    }
+  });
+  res.status(201).json({data:employee});
 };
 
 // TODO
 exports.createEmployee = async (req, res, next) => {
+  const {newname,newid}=req.body;
+  const newEmp ={
+    id : newid.toString(),
+    name : newname
+  };
+  const bool = employee.some(emp => emp.id === newEmp.id);
+  if (bool === true)
+  {
+    res.status(404).json({ error: 'this employee already exists'});
+  }
+  else 
+  {
+    employee.push(newEmp);
+    res.status(201).json({data:employee});
+  }
 };
